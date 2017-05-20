6039585f66b2f8b888497ff6eaadcd102e020446
Ben Kremer <ben.kremer@hotmail.co.uk>
2017-05-20 22:28:48 +0200

    Enhance styling, update bio

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
-(function () {
-	'use strict';
+(function() {
+  'use strict';
 
-	function generateMailTo() {
-		var mailto = 'mailto:' + 'ben' + '.' + 'kremer' + '@' + 'protonmail' + '.' + 'com';
-		var subject = '?subject=' + 'Hey Ben!';
-		return mailto + subject;
-	}
+  function generateMailTo() {
+    var mailto = 'mailto:' +
+    'ben' +
+    '.' +
+    'kremer' +
+    '@' +
+    'protonmail' +
+    '.' +
+    'com';
+    var subject = '?subject=' +
+    'Hey Ben!';
+    return mailto + subject;
+  }
 
-	$.easing.easeInOutQuint = function (x, t, b, c, d) {
-		if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
-		return c/2*((t-=2)*t*t*t*t + 2) + b;
-	};
+  $.easing.easeInOutQuint = function(x, t, b, c, d) {
+    if ((t /= d / 2) < 1)
+      return c / 2 * t * t * t * t * t + b;
+    return c / 2 * ((t -= 2) * t * t * t * t + 2) + b;
+  };
 
-	// smooth scroll
-	$('a[href*="#"]:not([href="#"])').click(function () {
-		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: target.offset().top
-				}, 1000, 'easeInOutQuint');
-				return false;
-			}
-		}
-	});
+  // smooth scroll
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length
+        ? target
+        : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, 'easeInOutQuint');
+        return false;
+      }
+    }
+  });
 
-	$('.email').attr('href', generateMailTo());
+  $('.email').attr('href', generateMailTo());
 
-	// $('.profile a').on('mouseover mouseout', function (e) {
-	// 	$(this).toggleClass('animated tada', e.type === 'mouseover');
-	// });
+  // $('.profile a').on('mouseover mouseout', function (e) {
+  // 	$(this).toggleClass('animated tada', e.type === 'mouseover');
+  // });
 
-	setTimeout(function () {
-		$('.credit').addClass('activate');
-	}, 1000);
+  setTimeout(function() {
+    $('.credit').addClass('activate');
+  }, 1000);
 })();
